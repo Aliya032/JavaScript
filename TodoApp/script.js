fcccd003aed9350cd6b62fcaf9c3b2be3bfb8ef7
Aliya <aliyahassan032@gmail.com>
2025-02-11 11:52:40 +0530

    LocalStroage - Todo App

---
+++ TodoApp/script.js
@@ -0,0 +1,2 @@
+// const taskData = []; /*this array will store all the tasks along with their associated data, including title, due date, & description. This storage will enable me to keep track of tasks, display them on the page, and save them to localStorage */
+// let currentTask = {}; /*This variable will be used t track the state when editing and discarding tasks.*/
